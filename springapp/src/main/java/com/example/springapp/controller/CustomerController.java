b14c1fa966fb06f99a9ae3761f2e17f3c4463703
raj-ad1tya <aditya09aeccse@gmail.com>
2023-06-30 12:26:56 +0000

    minor issue fixed

--- springapp/src/main/java/com/example/springapp/controller/CustomerController.java
+++ springapp/src/main/java/com/example/springapp/controller/CustomerController.java
@@ -21,11 +21,6 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
-
-	@GetMapping("/")
-	public String home() {
-		return "Welcome to food app";
-	}
 	
 	@PostMapping("/customer")
 	public String register(@RequestBody Customer customer) {
