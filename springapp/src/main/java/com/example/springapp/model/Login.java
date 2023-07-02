5635a4657592b59c13bf902fabcab73043e24cbb
urteju <kraviatp516@gmail.com>
2023-07-02 12:39:36 +0000

    #update

--- springapp/src/main/java/com/example/springapp/model/Login.java
+++ springapp/src/main/java/com/example/springapp/model/Login.java
@@ -1,6 +1,11 @@
 package main.java.com.example.springapp.model;
 
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="customers")
