1fe8d2b87c9e1eb4bf61f019d9adf4aef2a865cd
urteju <kraviatp516@gmail.com>
2023-07-02 17:21:15 +0000

    modifications in login

--- springapp/src/main/java/com/example/springapp/model/Login.java
+++ springapp/src/main/java/com/example/springapp/model/Login.java
@@ -1,7 +1,11 @@
 package com.example.springapp.model;
 
-
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="customers")
