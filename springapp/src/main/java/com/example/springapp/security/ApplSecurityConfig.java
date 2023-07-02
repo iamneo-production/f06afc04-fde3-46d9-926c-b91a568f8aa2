73cae0e06aa63c8fc1015ab92384e9955cb4a421
Ravi-Byahut <raviprasad725@gmail.com>
2023-07-02 12:44:54 +0000

    fixed minor issue

--- springapp/src/main/java/com/example/springapp/security/ApplSecurityConfig.java
+++ springapp/src/main/java/com/example/springapp/security/ApplSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.springapp.security;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class ApplSecurityConfig {
+	
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
