dc2a272adf0fc96354219cc1287936ba03323a43
urteju <kraviatp516@gmail.com>
2023-07-02 17:16:51 +0000

    modified Login files

---
+++ springapp/src/main/java/com/example/springapp/model/Login.java
@@ -0,0 +1,40 @@
+package com.example.springapp.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="customers")
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+
+
+    public Login() {
+
+    }
+
+    public Login(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
