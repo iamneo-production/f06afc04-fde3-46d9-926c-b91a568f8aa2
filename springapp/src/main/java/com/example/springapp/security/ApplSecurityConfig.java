bd86b5fd0bd200ace7773813f85162ffe000eeb6
Ravi-Byahut <raviprasad725@gmail.com>
2023-07-02 17:53:17 +0000

    Not neccesary

---
+++ springapp/src/main/java/com/example/springapp/security/ApplSecurityConfig.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplSecurityConfig {
+	
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf().disable()
+				.authorizeHttpRequests().antMatchers("/**").permitAll()
+				.and().build();
+	}
+}
