edb9aa8b0d7b98949ee038677d568493d90c5231
Ravi-Byahut <raviprasad725@gmail.com>
2023-07-02 17:53:17 +0000

    Not neccesary

--- springapp/src/main/java/com/example/springapp/security/ApplSecurityConfig.java
+++ springapp/src/main/java/com/example/springapp/security/ApplSecurityConfig.java
@@ -1,5 +1,3 @@
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
