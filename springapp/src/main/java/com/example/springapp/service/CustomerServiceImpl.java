b311fd0541f73c9f1b1f2f11b9911858126d7397
Ravi-Byahut <raviprasad725@gmail.com>
2023-07-02 12:44:54 +0000

    fixed minor issue

---
+++ springapp/src/main/java/com/example/springapp/service/CustomerServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.springapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springapp.model.Customer;
+import com.example.springapp.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+@Autowired
+	private CustomerRepository customerRepository;
+	
+	public String register(Customer customer) {
+		if(! customerRepository.existsByEmail(customer.getEmail())) {
+			customerRepository.save(customer);
+			return "Customer created";
+		}
+		return "Customer not created, email is already registered";
+	}
+	
+	public Customer getUserProfile(long id, String email, String password) {
+		if(customerRepository.existsByEmailAndPassword(email, password)) {
+			Customer customer = customerRepository.findById(id).orElse(null);
+			return customer;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	public String updateUserProfile(Customer customer) {
+		boolean customerExists = customerRepository.existsById(customer.getId());
+		if(customerExists) {
+			customerRepository.save(customer);
+			return "";
+		}
+		else {
+			return "No customer with the given id";
+		}
+	}
+}
