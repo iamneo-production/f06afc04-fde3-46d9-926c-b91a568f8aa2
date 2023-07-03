a03fc52c66477426d5b0ca8babdcffcc9fd06b9f
urteju <kraviatp516@gmail.com>
2023-07-03 12:18:38 +0000

    api 19 completed

--- springapp/src/main/java/com/example/springapp/controller/MenuItemController.java
+++ springapp/src/main/java/com/example/springapp/controller/MenuItemController.java
@@ -1,18 +1,19 @@
 package com.example.springapp.controller;
 
+import com.example.springapp.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.springapp.model.MenuItem;
 import com.example.springapp.service.MenuItemService;
 
-import java.awt.*;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping()
+@RequestMapping("/menu-item")
 public class MenuItemController {
+<<<<<<< HEAD
 	
 	@Autowired
 	private MenuItemService menuItemService;
@@ -34,3 +35,31 @@ public class MenuItemController {
 	}
 
 }
+=======
+
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @PostMapping
+    public String addMenuItem(@RequestBody MenuItem menuItem) {
+        String repsonse = menuItemService.addMenuItem(menuItem);
+        return repsonse;
+    }
+
+    @PutMapping
+    public String updateMenuItems(@RequestBody MenuItem menuItem) {
+        return menuItemService.updateMenuItems(menuItem);
+    }
+
+    @GetMapping
+    public List<MenuItem> getMenuItems() {
+        return menuItemService.getMenuItems();
+    }
+
+    @GetMapping("/{id}")
+    public MenuItem getMenuItemById(@PathVariable Long id) {
+        MenuItem menuItem = menuItemService.getMenuItemsById(id);
+        return menuItem;
+    }
+}
+>>>>>>> ee22ef3 (api 19 completed)
--- springapp/src/main/java/com/example/springapp/service/MenuItemService.java
+++ springapp/src/main/java/com/example/springapp/service/MenuItemService.java
@@ -1,5 +1,6 @@
 package com.example.springapp.service;
 
+import com.example.springapp.model.Order;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,27 +15,30 @@ import java.util.Optional;
 @Service
 public class MenuItemService {
 
-	@Autowired
-	private MenuItemRepository menuItemRepository;
-	
-	public String addMenuItem(MenuItem menuItem) {
-		menuItemRepository.save(menuItem);
-		return "Created";
-	}
-	public String updateMenuItems(MenuItem menuItem) {
-		Optional<MenuItem> menuItemOptional = menuItemRepository.findByName(menuItem.getName());
-		if(menuItemOptional.isPresent()){
-			menuItem.setId(menuItemOptional.get().getId());
-			BeanUtils.copyProperties(menuItem, menuItemOptional.get());
-			menuItemRepository.save(menuItemOptional.get());
-			return "Updated";
-		}
-		return "Not Updated";
-	}
+    @Autowired
+    private MenuItemRepository menuItemRepository;
 
-	public List<MenuItem> getMenuItems(){
-		return menuItemRepository.findAll();
-	}
+    public String addMenuItem(MenuItem menuItem) {
+        menuItemRepository.save(menuItem);
+        return "Created";
+    }
+    public String updateMenuItems(MenuItem menuItem) {
+        Optional<MenuItem> menuItemOptional = menuItemRepository.findByName(menuItem.getName());
+        if(menuItemOptional.isPresent()){
+            menuItem.setId(menuItemOptional.get().getId());
+            BeanUtils.copyProperties(menuItem, menuItemOptional.get());
+            menuItemRepository.save(menuItemOptional.get());
+            return "Updated";
+        }
+        return "Not Updated";
+    }
+
+    public List<MenuItem> getMenuItems(){
+        return menuItemRepository.findAll();
+    }
+
+    public MenuItem getMenuItemsById(Long id) {
+        return menuItemRepository.findById(id).orElse(null);
+    }
 
 }
-
