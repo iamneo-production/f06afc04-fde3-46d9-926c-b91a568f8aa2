6e84c6df19a557b8e4cc7a4528f1d03fdba9b141
urteju <kraviatp516@gmail.com>
2023-07-07 09:28:14 +0000

    deleted all files

--- springapp/src/main/java/com/example/springapp/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.example.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import main.java.com.example.springapp.model.Login;
-
-public interface LoginRepository extends JpaRepository<Login, Long> {
-    Login findByEmail(String email);
-
-}
--- springapp/src/main/java/com/example/springapp/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapp.repository;
-
-import com.example.springapp.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
