a63ad61b40d111d75ca829796ef970f03df9faad
Ravi-Byahut <raviprasad725@gmail.com>
2023-07-08 12:53:57 +0000

    imported annotation

--- springapp/src/main/java/com/example/springapp/controller/CustomerController.java
+++ springapp/src/main/java/com/example/springapp/controller/CustomerController.java
@@ -1,5 +1,8 @@
 package com.example.springapp.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
