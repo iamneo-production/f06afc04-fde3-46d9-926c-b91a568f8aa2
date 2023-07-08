91b1624a139a418aa44102f15fd6a4b9f57b8e03
urteju <kraviatp516@gmail.com>
2023-07-08 10:14:44 +0000

    login and logout modified to response entity

--- springapp/src/main/java/com/example/springapp/controller/LoginController.java
+++ springapp/src/main/java/com/example/springapp/controller/LoginController.java
@@ -4,6 +4,11 @@ import com.example.springapp.model.Login;
 import com.example.springapp.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+
 @RestController
 @RequestMapping("/")
 public class LoginController {
@@ -16,13 +21,17 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody Login login) {
-        return loginService.login(login.getEmail(), login.getPassword());
+    public ResponseEntity<String> login(@RequestBody Login login) {
+       String result = loginService.login(login.getEmail(), login.getPassword());
+       HttpStatus status = result.equals("Login successful") ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+       return ResponseEntity.status(status).body(result);
+  
     }
 
     @PostMapping("/logout")
-    public String logout() {
-        return loginService.logout();
+    public ResponseEntity<String> logout() {
+        String result = loginService.logout();
+        return ResponseEntity.ok(result);
     }
 
 
