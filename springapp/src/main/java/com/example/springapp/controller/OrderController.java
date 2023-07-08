98c925b8f2999c4d62c037d78b569505219b2039
Himaja323 <himajayaramothu007@gmail.com>
2023-07-08 16:26:32 +0000

    updated order status response

--- springapp/src/main/java/com/example/springapp/controller/OrderController.java
+++ springapp/src/main/java/com/example/springapp/controller/OrderController.java
@@ -50,9 +50,15 @@ public class OrderController {
     public Order getOrdersByRestaurantId(@PathVariable Long restaurantId) {
         return orderService.getOrdersByRestaurantId(restaurantId);
     }
-
-    @PutMapping("/{status}")
-    public Order updateOrderStatus(@RequestParam("orderId") Long orderId, @RequestParam("status") String status) {
-        return orderService.updateOrderStatus(orderId, status);
+    public ResponseEntity<String> updateOrderStatus(
+            @RequestParam("orderId") Long orderId,
+            @RequestParam("status") String status,
+            @RequestBody Order order) {
+        String result = orderService.updateOrderStatus(orderId, status, order);
+        if (result.equals("updated")) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
