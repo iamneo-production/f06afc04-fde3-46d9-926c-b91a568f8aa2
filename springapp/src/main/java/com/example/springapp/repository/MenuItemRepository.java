7d6578577e7af91e39765480f4a9124fd9b0968b
urteju <kraviatp516@gmail.com>
2023-07-09 10:20:21 +0000

    merging from main

--- springapp/src/main/java/com/example/springapp/repository/MenuItemRepository.java
+++ springapp/src/main/java/com/example/springapp/repository/MenuItemRepository.java
@@ -3,7 +3,17 @@ package com.example.springapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springapp.model.MenuItem;
+<<<<<<< HEAD
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
+=======
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+     Optional<MenuItem> findByName(String name);
+>>>>>>> origin
 }
