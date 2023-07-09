157700e1c9be1184e92d4aede8e560e6d8684955
Krishna170402 <krishnachandranrurk@gmail.com>
2023-07-09 14:41:32 +0000

    committed

--- springapp/src/main/java/com/example/springapp/service/RestaurantService.java
+++ springapp/src/main/java/com/example/springapp/service/RestaurantService.java
@@ -28,18 +28,19 @@ public class RestaurantService {
     public Restaurant getRestaurantById(Long id) {
         return restaurantRepository.findById(id).orElse(null);
     }
-    public  Restaurant restaurant(Restaurant restaurant){
-        long  id = restaurant.getId();
-       Restaurant res = restaurantRepository.findById(id).get();
-       res.setName(restaurant.getName());
-       res.setAddress(restaurant.getAddress());
-       res.setMenu_item_id(restaurant.getMenu_item_id());
-       return restaurantRepository.save(res);
- 
- 
-     }
-
+    
      public Restaurant findByRestaurantName(String name) {
     return restaurantRepository.findByName(name);
+}
+
+public String updateRestaurant(Restaurant restaurant) {
+    boolean restaurantExists = restaurantRepository.existsById(restaurant.getId());
+    if(restaurantExists) {
+        restaurantRepository.save(restaurant);
+        return "Restaurant updated successfully";
+    }
+    else {
+        return "No restaurant with the given id";
+    }
 }
 }
