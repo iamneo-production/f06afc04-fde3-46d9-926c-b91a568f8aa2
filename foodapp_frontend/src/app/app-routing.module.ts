ec75852372be6f1a12583025cd1786856d5ad35b
urteju <kraviatp516@gmail.com>
2023-07-10 14:47:13 +0000

    header, userlogin, dashboard was updated

---
+++ foodapp_frontend/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UsersignupComponent } from './usersignup/usersignup.component';
+import { AdminsignupComponent } from './adminsignup/adminsignup.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RestaurantloginComponent } from './restaurantlogin/restaurantlogin.component';
+import { RestaurantsignupComponent } from './restaurantsignup/restaurantsignup.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+
+  {path:'Header',component:HeaderComponent},
+  {path:'Dashboard',component:DashboardComponent},
+  {path:'Userlogin',component:UserloginComponent},
+  {path:'Usersignup',component:UsersignupComponent},
+  {path:'Adminlogin',component:AdminloginComponent},
+  {path:'Adminsignup',component:AdminsignupComponent},
+  {path:'Restaurantlogin',component:RestaurantloginComponent},
+  {path:'Restaurantsignup',component:RestaurantsignupComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Contactus',component:ContactusComponent},
+
+  
+  {path:'**',pathMatch:'full',redirectTo:'Dashboard'}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ foodapp_frontend/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {  FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UsersignupComponent } from './usersignup/usersignup.component';
+import { AdminsignupComponent } from './adminsignup/adminsignup.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RestaurantloginComponent } from './restaurantlogin/restaurantlogin.component';
+import { RestaurantsignupComponent } from './restaurantsignup/restaurantsignup.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    UserloginComponent,
+    UsersignupComponent,
+    AdminsignupComponent,
+    AdminloginComponent,
+    RestaurantloginComponent,
+    RestaurantsignupComponent,
+    ContactusComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
