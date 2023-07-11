58ca363f6fdb6ca78edb5bc27cfa296213840877
urteju <kraviatp516@gmail.com>
2023-07-11 16:42:37 +0000

    userlogin integration completed

--- foodapp_frontend/src/app/app.module.ts
+++ foodapp_frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,6 +34,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
 
   ],
--- foodapp_frontend/src/app/userlogin/userlogin.component.ts
+++ foodapp_frontend/src/app/userlogin/userlogin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userlogin',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./userlogin.component.css']
 })
 export class UserloginComponent {
+  email = '';
+  password = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(): void {
+    const loginData = { email: this.email, password: this.password };
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    this.http.post('http://localhost:8080/login', loginData, { headers, observe: 'response' }).subscribe(
+      (response) => {
+        if (response.status === 200) {
+          // Login successful, redirect to the Home component
+          this.router.navigate(['/Contactus']);
+        } 
+        },
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Show an alert for invalid credentials
+          alert('Invalid credentials. Please try again.');
+        } else {
+          console.log(error);
+        }
+      }
+    );
+  }
 }
