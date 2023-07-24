f309ed7e4890fe603ebecc75be7e14cae96cf6a4
Bharath-6 <veerasaibharath29@gmail.com>
2023-07-24 07:40:37 +0000

    home updated

---
+++ foodapp_frontend/src/app/restaurant.model.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+    cuisineType: any;
+    id: number;
+    name: string;
+    cuisinetype: string;
+    imageData: string;
+    // Add other properties related to the restaurant
+  }
+  
