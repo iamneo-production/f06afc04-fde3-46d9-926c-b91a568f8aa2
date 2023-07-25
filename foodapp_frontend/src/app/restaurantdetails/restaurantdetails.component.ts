43b862570b879c8f42c90f37ee153abf4baaf85a
Bharath-6 <veerasaibharath29@gmail.com>
2023-07-25 07:59:04 +0000

    now searching for food is working

---
+++ foodapp_frontend/src/app/restaurantdetails/restaurantdetails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantdetails',
+  templateUrl: './restaurantdetails.component.html',
+  styleUrls: ['./restaurantdetails.component.css']
+})
+export class RestaurantdetailsComponent {
+
+}
