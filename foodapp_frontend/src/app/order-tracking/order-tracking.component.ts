6e4ba33ca35d03dd4b588d6a6b7dea99e59fa6aa
Himaja323 <himajayaramothu007@gmail.com>
2023-07-25 08:23:51 +0000

    updated order-tracking

--- foodapp_frontend/src/app/order-tracking/order-tracking.component.ts
+++ foodapp_frontend/src/app/order-tracking/order-tracking.component.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 interface Order {
   id: number;
   customerId: number;
-  menuItemId: number;
   totalCost: number;
   deliveryAddress: string;
   deliveryTime: string;
@@ -28,7 +27,7 @@ export class OrderTrackingComponent implements OnInit {
 
   fetchOrderDetails() {
     const orderId = 1; 
-    this.http.get<Order>(`https://8080-aaabcaeedefcbacfcdbceaeaadbdbabf.project.examly.io/GET/Order`)
+    this.http.get<Order>(`https://8080-aaabcaeedefcbacfcdbceaeaadbdbabf.project.examly.io/GET_Order`)
       .subscribe(
         (response) => {
           this.order = response;
