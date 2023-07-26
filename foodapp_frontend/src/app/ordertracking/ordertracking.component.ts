73252057f19a87ff43fe54d31be79f1083d80911
Himaja323 <himajayaramothu007@gmail.com>
2023-07-26 04:52:59 +0000

    updated header to ordertracking

--- foodapp_frontend/src/app/ordertracking/ordertracking.component.ts
+++ foodapp_frontend/src/app/ordertracking/ordertracking.component.ts
@@ -28,7 +28,7 @@ export class OrdertrackingComponent implements OnInit {
 
   fetchOrderDetails() {
     const orderId = 1; 
-    this.http.get<Order>(`http://localhost:8080/order/${orderId}`)
+    this.http.get<Order>(`https://8080-aaabcaeedefcbacfcdbceaeaadbdbabf.project.examly.io/GET_Order`)
       .subscribe(
         (response) => {
           this.order = response;
