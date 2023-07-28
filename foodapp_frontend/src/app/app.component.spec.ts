0679b5c3ca9ac926780e0d1c57cfec22eee19795
Rangaraji <rangarajm24079@gmail.com>
2023-07-28 12:12:09 +0000

    latest update

--- foodapp_frontend/src/app/app.component.spec.ts
+++ foodapp_frontend/src/app/app.component.spec.ts
@@ -14,57 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-  it(`should have as title 'cart'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('cart');
-=======
-=======
->>>>>>> e632fe5 (frontebd)
->>>>>>> b981db2 (frontebd)
-=======
->>>>>>> 45103b0 (update for)
   it(`should have as title 'foodapp_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('foodapp_frontend');
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> ec75852372be6f1a12583025cd1786856d5ad35b
-=======
->>>>>>> e632fe5 (frontebd)
->>>>>>> b981db2 (frontebd)
-=======
->>>>>>> 45103b0 (update for)
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
-<<<<<<< HEAD
     expect(compiled.querySelector('.content span')?.textContent).toContain('foodapp_frontend app is running!');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cart app is running!');
-=======
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foodapp_frontend app is running!');
->>>>>>> ec75852372be6f1a12583025cd1786856d5ad35b
-=======
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foodapp_frontend app is running!');
->>>>>>> e632fe5 (frontebd)
->>>>>>> b981db2 (frontebd)
-=======
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foodapp_frontend app is running!');
->>>>>>> 45103b0 (update for)
   });
 });
--- foodapp_frontend/src/app/restaurant.model.ts
+++ foodapp_frontend/src/app/restaurant.model.ts
@@ -1,50 +1,12 @@
 export interface Restaurant {
-<<<<<<< HEAD
-<<<<<<< HEAD
     minimumordervalue: any;
     deliverytime: any;
     rating: any;
         cuisineType: any;
-=======
-<<<<<<< HEAD
-minimumordervalue: any;
-deliverytime: any;
-rating: any;
-    cuisineType: any;
-    id: number;
-    name: string;
-    cuisinetype: string;
-    imageData: string;
-    // Add other properties related to the restaurant
-  }
-  
-=======
-    rating: any;
-    deliverytime: any;
-    minimumordervalue: any;
->>>>>>> 4dcb710 (Update Restaurant)
-=======
-    minimumordervalue: any;
-    deliverytime: any;
-    rating: any;
-        cuisineType: any;
->>>>>>> 45103b0 (update for)
         id: number;
         name: string;
         cuisinetype: string;
         imageData: string;
-<<<<<<< HEAD
-<<<<<<< HEAD
         // Add other properties related to the restaurant
       }
       
-=======
-        // Add other properties related to the restaurant
-      }
->>>>>>> a7d4add (Update Restaurant)
->>>>>>> 4dcb710 (Update Restaurant)
-=======
-        // Add other properties related to the restaurant
-      }
-      
->>>>>>> 45103b0 (update for)
