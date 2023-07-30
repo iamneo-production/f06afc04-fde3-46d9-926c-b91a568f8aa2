c407a3cd2d63c98b27c2553e3aa19bc3f872deb8
Himaja323 <himajayaramothu007@gmail.com>
2023-07-30 15:26:51 +0000

    updated checkout, menu,restaurant details theme

--- foodapp_frontend/src/app/restaurantdetails/restaurantdetails.component.ts
+++ foodapp_frontend/src/app/restaurantdetails/restaurantdetails.component.ts
@@ -17,7 +17,11 @@ export class RestaurantdetailsComponent {
   id:number=0
   restaurant:any;
   ngOnInit(){
-    this.getRestaurntDetail(this.restaurantService.restaurantId)
+    this.route.paramMap.subscribe(params => {
+      this.id = +params.get('id')!;
+      console.log(this.id)
+      this.getRestaurntDetail(this.id)
+    });
   }
  getRestaurntDetail(id:number){
   this.restaurantService.getRestaurantById(id).subscribe(data=>{
